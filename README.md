a7bc450edaffc5434a6ce5136bbbf2ecf4e557dd
acoshift <acoshift@gmail.com>
2017-03-19 18:05:22 +0700

    add test and travis

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # mount
 
+[![Build Status](https://travis-ci.org/acoshift/mount.svg?branch=master)](https://travis-ci.org/acoshift/mount)
+[![Coverage Status](https://coveralls.io/repos/github/acoshift/mount/badge.svg?branch=master)](https://coveralls.io/github/acoshift/mount?branch=master)
+[![Go Report Card](https://goreportcard.com/badge/github.com/acoshift/mount)](https://goreportcard.com/report/github.com/acoshift/mount)
+[![GoDoc](https://godoc.org/github.com/acoshift/mount?status.svg)](https://godoc.org/github.com/acoshift/mount)
+
 Mount controllers and services into http handler in POST rpc style
---
+++ mount_test.go
@@ -0,0 +1,124 @@
+package mount
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func jsonBinder(r *http.Request, dst interface{}) error {
+	return json.NewDecoder(r.Body).Decode(dst)
+}
+
+type requestType struct {
+	Data int
+}
+
+func (req *requestType) Validate() error {
+	if req.Data < 1 {
+		return errors.New("invalid data")
+	}
+	return nil
+}
+
+func TestHandler(t *testing.T) {
+	var callSuccess, callError bool
+	successBody := &bytes.Buffer{}
+	errorBody := &bytes.Buffer{}
+	invalidBody := &bytes.Buffer{}
+
+	reset := func() {
+		callSuccess = false
+		callError = false
+		successBody.Reset()
+		successBody.WriteString("{\"data\": 1}")
+		errorBody.Reset()
+		errorBody.WriteString("{\"data\": 0}")
+		invalidBody.Reset()
+		invalidBody.WriteString("invalid")
+	}
+
+	m := New(Config{
+		Binder: jsonBinder,
+		SuccessHandler: func(w http.ResponseWriter, r *http.Request, res interface{}) {
+			callSuccess = true
+		},
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
+			callError = true
+		},
+	})
+
+	h := m.Handler(&requestType{}, func(ctx context.Context, req interface{}) (interface{}, error) {
+		r, ok := req.(*requestType)
+		if !ok {
+			t.Fatalf("invalid request type")
+		}
+		if r.Data != 1 {
+			t.Fatalf("invalid data")
+		}
+		return map[string]int{"ok": 1}, nil
+	})
+
+	r := httptest.NewRequest(http.MethodPost, "http://localhost", successBody)
+	w := httptest.NewRecorder()
+
+	reset()
+	h.ServeHTTP(w, r)
+	if !callSuccess {
+		t.Fatalf("success not call")
+	}
+	if callError {
+		t.Fatalf("error should not be called")
+	}
+
+	reset()
+	r = httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, r)
+	if callSuccess {
+		t.Fatalf("success should not be called")
+	}
+	if !callError {
+		t.Fatalf("error not call")
+	}
+
+	reset()
+	r = httptest.NewRequest(http.MethodPost, "http://localhost", errorBody)
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, r)
+	if callSuccess {
+		t.Fatalf("success should not be called")
+	}
+	if !callError {
+		t.Fatalf("error not call")
+	}
+
+	reset()
+	r = httptest.NewRequest(http.MethodPost, "http://localhost", invalidBody)
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, r)
+	if callSuccess {
+		t.Fatalf("success should not be called")
+	}
+	if !callError {
+		t.Fatalf("error not call")
+	}
+
+	h = m.Handler(&requestType{}, func(ctx context.Context, req interface{}) (interface{}, error) {
+		return nil, errors.New("some error")
+	})
+	r = httptest.NewRequest(http.MethodPost, "http://localhost", successBody)
+	w = httptest.NewRecorder()
+	reset()
+	h.ServeHTTP(w, r)
+	if callSuccess {
+		t.Fatalf("success should not be called")
+	}
+	if !callError {
+		t.Fatalf("error not call")
+	}
+}
